e558c73b21c47560b8a934d4ccbf5bfe44040e5d
Israel Barmack <israel.bar.dev@gmail.com>
2025-05-26 22:51:52 +0300

    feat: Add private_profile environment variable to settings and create new global modules

--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ load_dotenv()
 
 goto_url = os.getenv("GOTO_URL")
 profile = os.getenv("BROWSER_PROFILE")
+private_profile = os.getenv("PRIVATE_PROFILE")
 playwright_headless = True
 max_retries = 15
 retry_delay_sec = 5
