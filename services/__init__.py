946c4be31aa13501f838b066bbd029860488492e
Israel Barmack <israel.bar.dev@gmail.com>
2025-06-03 10:47:22 +0300

    feat: Remove Scheduler from exports in __init__.py and ensure proper inclusion of window and table

--- services/__init__.py
+++ services/__init__.py
@@ -1,12 +1,12 @@
 from .dao_service import TinyDatabase, QueryBuilder
 from .logging_service import Log
 from .web_access_service import WebAccess
-from .schedule_service import Scheduler
 from .frontend import window, table
 __all__ = [
     "TinyDatabase",
     "QueryBuilder",
     "Log",
     "WebAccess",
-    "Scheduler",
+    "window",
+    "table",
 ]
