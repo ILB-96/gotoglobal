a31b30e46044cf97617335a5c9ca2cb82cd236f8
Israel Barmack <israel.bar.dev@gmail.com>
2025-06-19 21:59:34 +0300

    refactor: Simplify worker initialization by consolidating setup into dedicated functions for WebDataWorker and WebAutomationWorker

--- src/app/app.py
+++ src/app/app.py
@@ -26,22 +26,25 @@ def start_app(app):
     account.to_json(settings.user_json_path)
     web_data_worker = WebDataWorker(account=account)
     web_automation_worker = WebAutomationWorker(account=account)
-    web_data_worker.start()
-    web_data_worker.request_otp_input.connect(lambda: handle_code_input(web_data_worker))
-    web_data_worker.input_send.connect(lambda data: web_automation_worker.set_location_data(data))
-    web_data_worker.input_received.connect(web_data_worker.set_account_data)
-    web_data_worker.page_loaded.connect(web_automation_worker.stop_event.set)
-    web_automation_worker.open_url_requested.connect(lambda url: web_data_worker.enqueue_url(url))
     
-    
+    create_web_data_worker(web_data_worker, web_automation_worker)
+
     create_web_automation_worker(main_win, tables, web_automation_worker)
-    web_automation_worker.request_pointer_location.connect(lambda query: web_data_worker.enqueue_pointer_location(query))
+    
     app.aboutToQuit.connect(web_automation_worker.stop)
     main_win.show()
     app.exec()
 
-def create_web_automation_worker(main_win, tables, worker):
-    
+def create_web_data_worker(worker, web_automation_worker):
+    worker.start()
+    worker.request_otp_input.connect(lambda: handle_code_input(worker))
+    worker.input_send.connect(lambda data: web_automation_worker.set_location_data(data))
+    worker.input_received.connect(worker.set_account_data)
+    worker.page_loaded.connect(web_automation_worker.stop_event.set)
+
+
+
+def create_web_automation_worker(main_win, tables, worker, web_data_worker):
     worker.start()
     worker.toast_signal.connect(main_win.show_toast)
     worker.late_table_row.connect(tables['late_rides'].add_rows)
@@ -50,4 +53,5 @@ def create_web_automation_worker(main_win, tables, worker):
     worker.input_received.connect(worker.set_account_data)
     worker.request_delete_table.connect(lambda tab, table: main_win.delete_table_from_tab(tab, table))
     worker.request_delete_tab.connect(lambda tab: main_win.delete_tab(tab))
-    return worker
+    worker.request_pointer_location.connect(lambda query: web_data_worker.enqueue_pointer_location(query))
+    worker.open_url_requested.connect(lambda url: web_data_worker.enqueue_url(url))
